d01e446e662839dbbb20991f541c28a0e629134a
francis <francis.gozdecki.work@gmail.com>
2025-05-24 11:42:16 +0200

    initial commit

---
+++ src/main/java/mas/fgozdecki/mas_25c_gozdecki_franciszek_s27379/Mas25cGozdeckiFranciszekS27379Application.java
@@ -0,0 +1,13 @@
+package mas.fgozdecki.mas_25c_gozdecki_franciszek_s27379;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mas25cGozdeckiFranciszekS27379Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mas25cGozdeckiFranciszekS27379Application.class, args);
+    }
+
+}
---
+++ src/test/java/mas/fgozdecki/mas_25c_gozdecki_franciszek_s27379/Mas25cGozdeckiFranciszekS27379ApplicationTests.java
@@ -0,0 +1,13 @@
+package mas.fgozdecki.mas_25c_gozdecki_franciszek_s27379;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mas25cGozdeckiFranciszekS27379ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
